93e71c8b6428d9e16fcd0d798e5b83ebca789241
Jason Hou <hjj1992@gmail.com>
2017-09-04 18:54:15 +0800

    faye

--- ruby/faye-websocket/config.ru
+++ ruby/faye-websocket/config.ru
@@ -1,6 +1,18 @@
 require 'bundler/setup'
 require_relative 'server'
+require 'stackprof'
+require 'ruby-prof'
 
-Faye::WebSocket.load_adapter('puma')
+# Faye::WebSocket.load_adapter('puma')
+Faye::WebSocket.load_adapter('thin')
 
-run Server.new
+#use StackProf::Middleware, enabled: true, mode: :object, interval: 1000, save_every: 5, raw: true
+#use Rack::RubyProf
+# result = RubyProf.profile do
+
+  run Server.new
+# end
+
+
+# printer = RubyProf::MultiPrinter.new(result)
+# printer.print(:path => "./tmp/profile", :profile => "profile")
--- ruby/faye-websocket/server.rb
+++ ruby/faye-websocket/server.rb
@@ -1,13 +1,35 @@
 require 'faye/websocket'
 require 'json'
 require 'set'
+require 'memory_profiler'
+require 'redis'
+
+def redis_connection()
+  Redis.new(host: '127.0.0.1', port: '6379') 
+end
 
 class Server
+
+  CHANNEL = "shootcut"
+  attr_accessor :subscriber
+
   def initialize
     @conns = Set.new
+    # @redis   = redis_connection
+
+    # Thread.new do
+    #   redis_sub = redis_connection
+    #   redis_sub.subscribe(CHANNEL) do |on|
+    #     on.message do |_, msg|
+    #       @conns.each {|ws| ws.send(msg) }
+    #     end
+    #   end
+    # end
+
   end
 
   def call(env)
+
     if Faye::WebSocket.websocket?(env)
       ws = Faye::WebSocket.new(env)
 
@@ -17,13 +39,17 @@ class Server
 
       ws.on :message do |event|
         cmd, payload = JSON(event.data).values_at('type', 'payload')
-        if cmd == 'echo'
-          ws.send({type: 'echo', payload: payload}.to_json)
-        else
-          msg = {type: 'broadcast', payload: payload}.to_json
-          @conns.each { |c| c.send(msg) }
-          ws.send({type: "broadcastResult", payload: payload}.to_json)
-        end
+        #report = MemoryProfiler.report do
+          if cmd == 'echo'
+            ws.send({type: 'echo', payload: payload}.to_json)
+          else
+            msg = {type: 'broadcast', payload: payload}.to_json
+            @conns.each { |c| c.send(msg) }
+            # @redis.publish(CHANNEL, msg)
+            ws.send({type: "broadcastResult", payload: payload}.to_json)
+          end
+        #end
+        #report.pretty_print(to_file: './tmp/broadcast_mem.txt')
       end
 
       ws.on :close do |event|
